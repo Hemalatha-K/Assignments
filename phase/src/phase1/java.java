42ffc7c2c87b0292ef59d36087c007d1c6a3da08
Hemalatha-K <hema18ec039@rmkcet.ac.in>
2022-07-24 20:01:24 +0530

    First Commit

--- phase/src/phase1/java.java
+++ phase/src/phase1/java.java
@@ -1,5 +1,42 @@
 package phase1;
 
 public class java {
+		
+		public int calculate(int a, int b)
+		{	
+			int c=a+b;
+			return c;
+		}
+		public int calculate(int a)
+		{	
+			int m= (int)3.14*a*a;
+			return m;
+		}
+		public int calculate(float c, float d)
+		{	
+			int m = (int) ((int)c*d);
+			return m;
+		}
+		public int calculate(float d)
+		{	
+			int j= (int)((int)d*d);
+			return j;
+		}
+		
 
-}
+		public static void main(String[] args) {
+			java C = new java();
+			int i=C.calculate(2,6);
+			int j=C.calculate(1);
+			int k=C.calculate(2f,6f);
+			int l=C.calculate(2f);
+			
+			System.out.println("Add Two Numbers="+ i);
+			System.out.println("Area of circle="+ j);
+			System.out.println("Area of rectangle="+ k);
+			System.out.println("Area of square="+ l);
+		}
+
+	}
+
+
